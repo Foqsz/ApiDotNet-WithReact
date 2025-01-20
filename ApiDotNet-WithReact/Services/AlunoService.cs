5d67d4048b87b53e730e34a0093c71a17b1e7cb6
Foqsz <contatovictorvinicius05@gmail.com>
2025-01-20 11:07:14 -0300

    feat: Service e interface. Bem basico, apenas para praticar React.

---
+++ ApiDotNet-WithReact/Services/AlunoService.cs
@@ -0,0 +1,66 @@
+﻿using ApiDotNet_WithReact.Context;
+using ApiDotNet_WithReact.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDotNet_WithReact.Services
+{
+    public class AlunoService : IAlunoService
+    {
+        private readonly AppDbContext _context;
+
+        public AlunoService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Aluno>> GetAlunos()
+        {
+            try
+            {
+                return await _context.Alunos.ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Aluno>> GetAlunosByName(string nome)
+        {
+            IEnumerable<Aluno> alunos;
+            if (!string.IsNullOrEmpty(nome))
+            {
+                alunos = await _context.Alunos.Where(a => a.Nome.Contains(nome)).ToListAsync();
+            }
+            else
+            {
+                alunos = await GetAlunos();
+            }
+            return alunos;
+        }
+
+        public async Task<Aluno> GetAluno(int id)
+        {
+            var alunos = await _context.Alunos.FindAsync(id);
+            return alunos;
+        }
+
+        public async Task CreateAluno(Aluno aluno)
+        {
+            await _context.Alunos.AddAsync(aluno);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAluno(Aluno aluno)
+        {
+            _context.Entry(aluno).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAluno(Aluno aluno)
+        {
+            _context.Alunos.Remove(aluno);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ApiDotNet-WithReact/Services/IAlunoService.cs
@@ -0,0 +1,14 @@
+﻿using ApiDotNet_WithReact.Models;
+
+namespace ApiDotNet_WithReact.Services;
+
+public interface IAlunoService
+{
+    //Task = operação assincrona. 
+    Task<IEnumerable<Aluno>> GetAlunos();
+    Task<Aluno> GetAluno(int id);
+    Task<IEnumerable<Aluno>> GetAlunosByName(string nome);
+    Task CreateAluno(Aluno aluno);
+    Task UpdateAluno(Aluno aluno);
+    Task DeleteAluno(Aluno aluno);
+}
