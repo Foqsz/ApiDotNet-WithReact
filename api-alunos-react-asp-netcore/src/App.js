076889a4820d6e1ad81205eb668b73d04ab93d25
Foqsz <contatovictorvinicius05@gmail.com>
2025-01-23 10:50:39 -0300

    feat: referenciando recursos que irei utilizar

--- api-alunos-react-asp-netcore/src/App.js
+++ api-alunos-react-asp-netcore/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import logoCadastro from '.assets/logoCadastro.png';
+
 function App() {
   return (
     <div className="App">
