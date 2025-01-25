c9e20951c2b5fbd8554d249048b6f7af077028c3
Foqsz <contatovictorvinicius05@gmail.com>
2025-01-25 11:22:21 -0300

    feat: model de login

---
+++ ApiDotNet-WithReact/ViewModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDotNet_WithReact.ViewModels;
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "O e-mail é obrigatório.")]
+    [EmailAddress(ErrorMessage = "E-mail inválido.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "A senha é obrigatória.")]
+    [StringLength(20, ErrorMessage = "A {0} deve ter no mínimo {2} e no máximo {1} caracteres", MinimumLength = 10)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
