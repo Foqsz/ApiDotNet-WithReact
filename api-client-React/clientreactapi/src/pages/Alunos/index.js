c6cb05daeeda63bf368c29f909608c4de4d013e7
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-04 10:36:33 -0300

    feat: metodo para logout

--- api-client-React/clientreactapi/src/pages/Alunos/index.js
+++ api-client-React/clientreactapi/src/pages/Alunos/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect, use} from 'react';
+import api from '../../services/api';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import logoCadastro from '../../assets/cadastro.png';
 
@@ -7,63 +8,80 @@ import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
 
 
 export default function Alunos() {
+
+    const[nome, setNome] = useState('');
+    const[alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const history = useHistory();
+
+    const authorization = 
+    {
+        headers :
+        {
+            Authorization : `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => 
+    {
+        api.get('/api/Alunos', authorization).then(
+            response => 
+            {
+                setAlunos(response.data);
+            }, token) 
+    })
+
+    async function logout()
+    {
+        try
+        {
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = '';
+            history.push('/');
+        }
+        catch (error)
+        {
+            alert('Erro ao realizar logout, tente novamente' + error);
+        }
+    }
     return(
         <div className="alunos-container">
             <header>
                 <img class="logo" src={logoCadastro} alt="Cadastro"/>  
-                <span> Bem-Vindo, <strong>Foqs</strong>!</span>
+                <span> Bem-Vindo,<strong>{email}!</strong></span>
                 <br></br>
-                <Link class="button" to="aluno/novo/0">Novo Aluno</Link>
-                <button class="buttonX" type="button">
+                <Link class="buttonAluno" to="aluno/novo/0">Novo Aluno</Link>
+                <button onClick={logout} class="buttonX" type="button">
                     <FiXCircle size={35} color="#17202a"/>
                 </button>
             </header>
             <form>
-                <input type="text" placeholder="Nome"/>
-                <button type="button" class="button">
+                <input class="nomeClass" type="text" placeholder="Nome"/>
+                <button type="button" class="buttonFiltro">
                     Filtrar aluno por nome (parcial)
                 </button>
             </form>
             <h1>Relação de Alunos</h1>  
-            <ul class="alunos-list">
-                <li>
-                <b>Nome: </b> Paulo<br></br>
-                <b>Email: </b> paulo@email.com<br></br>
-                <b>Idade: </b> 22<br></br>
-                <button type="button">
-                    <FiEdit size="25" color="#17202a"/>
-                </button>
-                <button type="button">
-                    <FiUserX size="25" color="#17202a"/>
-                </button>
-                </li>
+            <ul class="alunos-list"> 
+                {alunos.map(aluno =>(
+                    <li key={aluno.id}>
+                    <b>Nome: </b>{aluno.nome}<br></br>
+                    <b>Email: </b>{aluno.email}<br></br>
+                    <b>Idade: </b>{aluno.idade}<br></br>                 
 
-                <li>
-                <b>Nome: </b> Paulo<br></br>
-                <b>Email: </b> paulo@email.com<br></br>
-                <b>Idade: </b> 22<br></br>
-                <button type="button">
-                    <FiEdit size="25" color="#17202a"/>
-                </button>
-                <button type="button">
-                    <FiUserX size="25" color="#17202a"/>
-                </button>
-                </li>
-
-                <li>
-                <b>Nome: </b> Paulo<br></br>
-                <b>Email: </b> paulo@email.com<br></br>
-                <b>Idade: </b> 22<br></br>
-                <button type="button">
-                    <FiEdit size="25" color="#17202a"/>
-                </button>
-                <button type="button">
-                    <FiUserX size="25" color="#17202a"/>
-                </button>
-                </li>
-
-                
-            </ul>
-            </div>
+                    <button type="button">
+                        <FiEdit size="25" color="#17202a"/>
+                    </button>
+                    <button type="button">
+                        <FiUserX size="25" color="#17202a"/>
+                    </button>
+                    </li>
+                    ))}
+            </ul>    
+        </div>
     )
 }
