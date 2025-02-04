b707a1c07e6971a25744dac7b2b42525b3bbb442
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-04 09:39:20 -0300

    feat: consumindo o metodo de login com o axios

--- api-client-React/clientreactapi/src/pages/login/index.js
+++ api-client-React/clientreactapi/src/pages/login/index.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
+import api from '../../services/api';
+import {useHistory} from 'react-router-dom';
+
 import logoImage from '../../assets/logo.png';
 
 export default function Login()
 {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function login(event)
+    {
+        event.preventDefault();
+
+        const data = { email, password };
+
+        try
+        {
+            const response = await api.post('/api/Account/LoginUser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            history.push('/alunos');
+        }
+        catch(error)
+        {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return(
         <div className="login-container">
             <section className="form">
                 <img src={logoImage} alt="login" id="img1" style={{ width: '150px', height: 'auto' }} />
-                <form>
+                <form onSubmit={login}>
                     <h1>Cadastro de Alunos</h1>
-                    <input placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                    />
+                    
+                    <input type="password" placeholder="Password" 
+                        value={password}
+                        onChange={e=>setPassword(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Login</button>
+
                 </form>
             </section>
         </div>
